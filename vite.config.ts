e17a1824a3e562242cce6f813c223185625edae8
sonpablo <pablorodriguez833@gmail.com>
2025-03-05 00:04:36 +0100

    feat: implements production mode

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    minify: 'terser',
+    cssCodeSplit: true,
+    assetsInlineLimit: 4096,
+    sourcemap: false,
+  },
+  server: {
+    open: true,
+  },
 });
